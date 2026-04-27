5a1f20561339b484d8dca0d9c1ebeadc1efc53cb
Natalia <nataliadiasfernande@gmail.com>
2026-04-27 15:09:57 -0300

    Conversão de tipos

---
+++ src/Variaveis/ConversaoTipos.kt
@@ -0,0 +1,23 @@
+package Variaveis
+
+fun main(){
+    val int1 = 20
+    val long1 = int1.toLong()
+    println(int1)
+    println(long1)
+
+    val double1 =int1.toDouble()
+    println(double1)
+
+    val double2 = 34.91
+    val int2 = double2.toInt()
+    println(double2)
+    println(int2)
+
+    val string1 = int1.toString()
+    val string2 = "101"
+    val int3 = string2.toInt()
+
+    println(string1)
+    println(int3)
+}
