488849de0ccaa253e8e80e901a3b88df79556bac
Natalia <nataliadiasfernande@gmail.com>
2026-04-27 14:49:51 -0300

    Tipos de dados

---
+++ src/Variaveis/TiposDeDados.kt
@@ -0,0 +1,31 @@
+package Variaveis
+
+fun main(){
+    //Tipo númericos
+    val byte: Byte = 8
+    val short: Short = 360
+    val int: Int = 1000
+    val long: Long = 10000L
+    val float: Float = 150.10F
+    val double: Double = 300.10
+
+    println(byte)
+    println(short)
+    println(int)
+    println(long)
+    println(float)
+    println(double)
+
+    //Tipo Booleano
+    //True False
+    val boolean: Boolean = false
+
+    println(boolean)
+
+    //Tipos textuais
+    val char: Char = 'F'
+    val string: String = "Fonseca"
+
+    println(char)
+    println(string)
+}
