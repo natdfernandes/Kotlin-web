16b23fd2800cf9f00a1ccf5ab401c1647b59b5eb
Natalia <nataliadiasfernande@gmail.com>
2026-04-29 14:22:08 -0300

    Operadores aritmeticos

---
+++ src/Operadores/Aritmeticos.kt
@@ -0,0 +1,25 @@
+package Operadores
+
+fun main(){
+    val a = 7        //Altere para 7.0 e veja o resultado. Double
+    val b = 2
+
+    //Soma
+    println(a + b)
+
+    //Subtração
+    println(a - b)
+
+    //Multiplicação
+    println(a * b)
+
+    //Divisão
+    println(a / b)
+
+    //Modulo
+    println(a % b)
+
+    //Concatenação
+    val c ="Jose" + " Erinaldo"
+    println(c)
+}
