849af7e5da6b343073c7ef2d64bd1d187adda316
Natalia <nataliadiasfernande@gmail.com>
2026-04-29 14:35:02 -0300

    Operadores Relacionais

---
+++ src/Operadores/Relacionais.kt
@@ -0,0 +1,25 @@
+package Operadores
+
+fun main(){
+    val a = 12
+    val b = 8
+    val c = 8
+
+    //Maior que
+    println(a > b)
+
+    //Menor que
+    println(a < b)
+
+    //Maior ou igual
+    println(c >= a)
+
+    //Menor ou igual
+    println(c <= a)
+
+    //Igual
+    println(b == c)
+
+    //Diferente
+    println(b != b)
+}
