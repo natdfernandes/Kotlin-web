908ea6bd11a480d5eb5b2f8e1808a7badc090929
Natalia <nataliadiasfernande@gmail.com>
2026-05-06 11:00:38 -0300

    Strings literal

---
+++ src/Strings/Literal.kt
@@ -0,0 +1,14 @@
+package Strings
+
+fun main(){
+    val name = "Jose Erinaldo"
+    println(name)
+
+    val poema = """
+        Rosa é vermelha
+        Violeta é azul
+        Kotlin é moderno
+        E o código é legal!
+    """.trimIndent()
+    println(poema)
+}
