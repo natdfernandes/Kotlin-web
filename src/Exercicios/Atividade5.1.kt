afd64389fe39b5e406de7c0feb529ac9b18e0c29
Natalia <nataliadiasfernande@gmail.com>
2026-05-06 14:40:37 -0300

    Atividade 5.1

---
+++ src/Exercicios/Atividade5.1.kt
@@ -0,0 +1,14 @@
+package Exercicios
+
+// Você foi contratado por uma escola para ajudar na construção de um sistema simples de alunos
+//Esse sistema deve receber informações básicas e formatá-las para impressão no boletim
+//Crie um programa em Kotlin que:
+//1. leia o nome completo do aluno(pode vir com letras minúsculas e espaços extras)
+//2. leia o curso que o aluno está matriculado
+//3. leia o email institucional
+//4.Padronize e exiba os dados do aluno seguindo as regras
+//Regras para o programa:
+// O nome deve ser impresso com a primeira letra maiscúlas
+//O e-mail deve ser impresso como está
+//Mostre o número total de caracteres do nome (sem contar os espaços extras)
+//Use string templates para montar a frase final
