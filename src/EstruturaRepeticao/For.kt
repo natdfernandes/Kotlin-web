5e7601c92b89bafb6b525062d847f9c5e7357bd3
Natalia <nataliadiasfernande@gmail.com>
2026-05-06 09:56:22 -0300

    Aprendendo For

---
+++ src/EstruturaRepeticao/For.kt
@@ -0,0 +1,15 @@
+package EstruturaRepeticao
+
+fun main(){
+    for (x in 1 .. 10 step 2){
+        println(x)
+    }
+
+    // for (x in 1..10){
+    //  println(x)
+    //}
+
+    //for (x in 10 downTo 1){
+    //  println(x)
+    // }
+}
