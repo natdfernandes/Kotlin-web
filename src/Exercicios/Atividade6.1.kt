e46dc1d7577d00f0a6f07c3b048505676bf91a9d
Natalia <nataliadiasfernande@gmail.com>
2026-05-08 12:39:38 -0300

    Atividade 6.1

---
+++ src/Exercicios/Atividade6.1.kt
@@ -0,0 +1,9 @@
+package Exercicios
+
+//Uma escola desenvolveu um sistema simples para cadastrar a nota de um aluno em determinada disciplina
+// O sistema deve:
+// 1. Receber como entrada a nota do aluno, que pode ser nula (ou seja, o aluno pode não ter feito a prova ainda)
+// 2. Usar o call operator(?.) para verificar se a nota não é nula antes de exibir o valor
+// 3. Caso a nota informada seja mnor que 0 ou maior que 10, o programa deve lançar uma IllegalArgumentException,
+// já que não existe nota fora desse intervalo
+// Sua tarefa é implementar um código simples em Kotlin que atenda a esses requisitos
