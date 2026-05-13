56ed71e870778bb07bfce11d15d81e6249b2c632
Natalia <nataliadiasfernande@gmail.com>
2026-05-13 11:15:34 -0300

    Modulo 8 - atividade8.1

---
+++ src/Exercicios/Atividade8.1.kt
@@ -0,0 +1,14 @@
+package Exercicios
+
+// Uma loja de fruta deseja controlar o estoque de seus produtos
+// Os produtos disponiveis são:
+// "Maça" = 5, "banana" = 2, "laranja" = 0, "uva" = 0, "abacaxi" = 3
+// A loja precisa:
+// Registrador de estoque inicial de cada fruta (quantidade em unidades)
+// Atualizar o estoque quando chegam novas frutas
+//Listar quais frutas estão em falta (quantifdade 0)
+// Saber quais frutas estão disponiveis no estoque (quantidade > 0)
+// Tarefas:
+// Crie as coleções selecionadas para representar o estoque da loja
+// Atualize o estoque adicionando 10 maças, 5 uvas e retirando 2 bananas
+// Liste todas as frutas disponiveis e as frutas que faltam
